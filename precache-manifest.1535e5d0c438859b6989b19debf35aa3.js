4ae7a8e266101d28a9104431f9a2ba42a59d995a
박지현 <parkji2028@naver.com>
2020-02-19 20:08:40 +0900

    first commit

---
+++ precache-manifest.1535e5d0c438859b6989b19debf35aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616125a9c8e05288b9f2462034ef9faf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6963dea5b3f3abb960",
+    "url": "/static/css/main.61ec581e.chunk.css"
+  },
+  {
+    "revision": "0408ce7a0ff4c9e50fbd",
+    "url": "/static/js/2.ea5d773a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ea5d773a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6963dea5b3f3abb960",
+    "url": "/static/js/main.9ec5db58.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  }
+]);
